3d61e0a045956e1eb4f622678d3da500668ad3a6
Gurpreet Singh <108724695+Agnidaah@users.noreply.github.com>
2023-12-10 16:26:17 +0530

    Add files via upload

---
+++ db/config.js
@@ -0,0 +1,3 @@
+const mongoose=require('mongoose');
+mongoose.set('strictQuery',false);
+mongoose.connect("mongodb+srv://gurpreetsinghf22:0HeLmklCbKztvz4Y@cluster0.g3xlq3o.mongodb.net/Crypto?retryWrites=true&w=majority");
